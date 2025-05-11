cda7fdf15d5b327cb438fa3853b03e18b668a754
Akmal009 <107461237+Akmal009@users.noreply.github.com>
2025-05-11 15:58:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 const tg = window.Telegram.WebApp;
-
-tg.ready();
 tg.expand();
 
-function initApp() {
-  const user = tg.initDataUnsafe.user?.first_name || "гость";
-  document.getElementById("user").innerText = `Привет, ${user}!`;
-
-  document.getElementById("loader").classList.add("hidden");
-  document.getElementById("content").classList.remove("hidden");
-}
-
-window.addEventListener("load", () => {
-  setTimeout(initApp, 5); // здесь можешь заменить на fetch/инициализацию
-});
+document.getElementById("user").innerText =
+  `Привет, ${tg.initDataUnsafe.user?.first_name || "гость"}!`;
